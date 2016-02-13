d2439b7ea9943b6bdb6d471e3203896acce48ad2
bartal veyhe <bartal35@gmail.com>
2016-02-13 17:35:55 +0000

    Clearn up the main script in index, by movie stuff into function in utils
    update so that input will update the amount of chars left before haveing to split the message.
    
    also update so that add in new messages is easire and made the selection dynamic so that update the list of message makes it eaire to have the select working
    
    Add in the split and join dialogs with out functions

--- Script/util.js
+++ Script/util.js
@@ -1,188 +1,90 @@
 var charSplit = "<b class='charSplit'>|</b>";
+var maxCharLenght = 60;
 
-function openWindow($title, $content, callback) {
-    var winFeature =
-        'location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes; width:500px, height:500px';
-    var myWindow = window.open("", $title, winFeature);
-    myWindow.document.write($content);
-    $(myWindow).bind('beforeunload', callback || function (e) {
-        });
-    return myWindow;
+
+function renderWheel() {
+    var wheel = new wheelnav("wheelDiv");
+    wheel.slicePathFunction = slicePath().DonutSlice;
+    wheel.createWheel(["Encryption", "Delete", "Checksum", "Modify"]);
+    wheel.titleAttr = {fill: "#111"};
+    wheel.titleSelectedAttr = {fill: "#FFF"};
+    wheel.titleHoverAttr = {fill: "#FFF"};
+    wheel.clockwise = false;
+    wheel.clickModeRotate = false;
+    wheel.refreshWheel();
 }
 
-function updateSplitMarker(content, splitsize) {
-    var regexSplitsize = new RegExp(".{1," + splitsize + "}", "g");
-    console.log(content.match(regexSplitsize));
-}
 
-function createColorCodingInput() {
-
-}
-
-function getInput() {
-}
-
-function openSplitWindow(message) {
-    var content = '<html>' +
-        '<head>' +
-        '<link rel="stylesheet" href="Style/bootstrap.min.css">' +
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">' +
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">' +
-        '<script>' +
-        '$(text_edit).prop("contentEditable", true).blur(function () {' +
-        '        var chars = $(this).text().split("");' +
-        '        $(inputID).val($(this).text());' +
-        '' +
-        '        this.innerHTML = "";' +
-        '' +
-        '        console.log(updateSplitMarker(chars.join(""), 20));' +
-        '        $.each(chars, function () {' +
-        '            $("<span>").text(this).css({' +
-        '                color: "#" + (Math.random() * 16777215 | 0).toString(16)' +
-        '            }).appendTo(text_edit);' +
-        '        });' +
-        '    });' +
-        '</script>' +
-        '<style>' +
-        '#text_edit{' +
-        'height:200px;' +
-        'width: 100%;' +
-        '}' +
-        '</style>' +
-        '</head>' +
-        '<body>' +
-        '<div class="contaner-fluid">' +
-        '	<div class="row">' +
-        '		<div class="col-xs-12">' +
-        '           <div id="text_edit">' + message + '</div>' +
-        '           <input type="hidden" id="spintSend"/>' +
-        '		</div>' +
-        '	</div>' +
-        '	<div class="row">' +
-        '		<div class="col-xs-12">' +
-        '			<button type="submit" class="btn btn-default">send incident</button>' +
-        '			<button type="submit" class="btn btn-default">cansel</button>' +
-        '		</div>' +
-        '	</div>' +
-        '</div>' +
-        '</body>' +
-        '</html>';
-
-    var window = openWindow("Split", content, function (e) {
-        console.log(e)
+function createSortableListsOnJoin() {
+    $("#toJoin").sortable({
+        connectWith: "ul"
     });
 
+    $("#avalibleToJoin").sortable({
+        connectWith: "ul",
+        dropOnEmpty: false
+    });
+
+    $("#toJoin, #avalibleToJoin").disableSelection();
 }
 
 
-function openJoinWindow(message) {
-    var content = '<html>' +
-        '<head>' +
-        '<link rel="stylesheet" href="Style/bootstrap.min.css">' +
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">' +
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">' +
-        '</head>' +
-        '<body>' +
-        '<div class="contaner-fluid">' +
-        '	<div class="row">' +
-        '		<div class="col-sm-6">' +
-        '    <div class="list-group" style="max-height: 80vh; overflow-y:scroll;">' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum t eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '</div>' +
-        '		</div>' +
-        '		<div class="col-sm-6">' +
-        '    <div class="list-group" style="max-height: 80vh; overflow-y:scroll;">' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum t eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '        <a href="#" class="list-group-item">Cras justo odio</a>' +
-        '        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>' +
-        '        <a href="#" class="list-group-item">Morbi leo risus</a>' +
-        '        <a href="#" class="list-group-item">Porta ac consectetur ac</a>' +
-        '        <a href="#" class="list-group-item">Vestibulum at eros</a>' +
-        '    </div>' +
-        '		</div>' +
-        '	</div>' +
-        '	<div class="row">' +
-        '		<div class="col-xs-12">' +
-        '			<button type="submit" class="btn btn-default">send incident</button>' +
-        '			<button type="submit" class="btn btn-default">cansel</button>' +
-        '		</div>' +
-        '	</div>' +
-        '</div>' +
-        '</body>' +
-        '</html>';
+function openSplitMessage() {
+    var input = $('#messageInput').val();
+    $('#spiltMessageTextEditor').html(input);
+    $('#splitModal').modal('toggle');
+}
 
-    var window = openWindow("Split", content, function (e) {
-        console.log(e)
-    });
-
+function sendMessageButtonHandler() {
+    console.log("testing");
+    if ($(this).hasClass('send')) {
+        var input = $('#messageInput').val();
+        $('#messageInput').val('');
+        addMessageToMessageList(input);
+    } else {
+        openSplitMessage();
+    }
 }
 
 
-function createContentEditAria(conentID, inputID) {
-    $(conentID).prop("contentEditable", true).blur(function () {
-        var chars = $(this).text().split("");
-        $(inputID).val($(this).text());
+function bodyMouseHandler(event) {
+    switch (event.which) {
+        case 3:
+            // get moues possion, set possion to new possion
+            // set visiblilty to true
+            console.log(event.clientX, event.clientY);
+            $('#wheelDiv').css({
+                position: "absolute",
+                marginLeft: 0, marginTop: 0,
+                top: event.clientY - 130, left: event.clientX - 130
+            });
+            $('#wheelDiv').fadeIn('slow');
+            break;
+        case 1:
+            $('#wheelDiv').fadeOut('fast');
+        default:
+    }
+}
 
-        this.innerHTML = "";
+function inputKeybordHandler(evnet) {
+    var content = $(this).val();
+    var maxChar = maxCharLenght;
+    var charLable = "";
 
-        console.log(updateSplitMarker(chars.join(""), 20));
-        $.each(chars, function () {
-            $("<span>").text(this).css({
-                color: "#" + (Math.random() * 16777215 | 0).toString(16)
-            }).appendTo(conentID);
-        });
-    });
+    if (content.length <= maxChar) {
+        charLable = "You have " + (maxChar - content.length) + " left";
+        $("#sendMessage").addClass('send');
+        $("#sendMessage").html("Sent message");
+    } else {
+        charLable = "You will have to split the message to send of the message";
+        $("#sendMessage").addClass('split');
+        $("#sendMessage").html("split message");
+    }
+    $('#messageLabel').html(charLable);
+
+}
+
+function addMessageToMessageList(message) {
+    var htmlMessage = '<li href="#" class="list-group-item">' + message + '</li>';
+    $('#messageBord').append(htmlMessage);
 }
