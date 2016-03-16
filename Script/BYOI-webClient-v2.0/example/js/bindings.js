fd3646212d6472ce9ab110706736ef16a236ea5a
bartal veyhe <bartal35@gmail.com>
2016-03-16 23:38:32 +0000

    Update to use the build in API example, got most of the function working as plannd

--- Script/BYOI-webClient-v2.0/example/js/bindings.js
+++ Script/BYOI-webClient-v2.0/example/js/bindings.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //bind elements of the DOM to BYOI methods
-    
-    $('#connectButton').click(function(){
+
+    $('#connectButton').click(function () {
         // attempt connection to the server
         BYOI.connect();
     });
 
 
-    $('#deleteButton').click(function(){
+    $('#deleteButton').click(function () {
         // delete all selected messages from the main Message Handler
         $('#messageList').getSelectedMessages().remove();
     });
 
-    $('#addButton').click(function(){
+    $('#addButton').click(function () {
         //this is just an example, the final html should be defined by the 
         //student, all the meta-data should be added here though
         //
@@ -22,71 +22,94 @@ $(document).ready(function() {
         //use this part of the API is shown in the connection.js file
         $('#messageList').getSelectedMessages().toggleSelectMessage();
         var msg = $('<div class="added"><span class="text">' + $('#msg').val() + '</span>&nbsp;</div>').BYOIMessage();
-        msg.relayMessage()
-            .css('background', 'green')
-            .animate(
-                {'background-color':'transparent'}, 
-                'slow', 
-                'swing', 
-                function(){$(this).removeAttr('style');
-            });
+        msg.relayMessage();
     });
 
 
     // enable click selection of messages
-    $(document).on('click', '.BYOI-message', function(){// bind to every BYOI message, regardless of their Message Handler
-        // toggle selection
-        $(this).toggleSelectMessage();
-        // set the input value to the selected message's text 
-        $('#msg').val($(this).data('text'));
+    //$(document).on('click', '.BYOI-message', function () {// bind to every BYOI message, regardless of their Message Handler
+    //    // toggle selection
+    //    $(this).toggleSelectMessage();
+    //    // set the input value to the selected message's text
+    //    $('#msg').val($(this).data('text'));
+    //});
+
+    $(document).ready(function () {
+        $("#messageBord").sortable({
+            connectWith: "div",
+            dropOnEmpty: true,
+
+            update: function (event, ui) {
+                console.log(event);
+            }
+        });
+        $("#messageBord").disableSelection();
+        var toJoinDarg = false;
+
+
+        $('#messageBord').on('mouseup', '.BYOI-message', function () {
+            var wasDrag = toJoinDarg;
+            toJoinDarg = false;
+            if (!wasDrag) {
+                $(this).toggleSelectMessage();
+            }
+        }).on('mousedown', '.BYOI-message', function () {
+            toJoinDarg = false;
+        }).on('mousemove', '.BYOI-message', function () {
+            toJoinDarg = true;
+        });
     });
 
     // bind combine method to the message handler
-    $('#combineButton').click(function(){
-        $('#messageList').combineMessages(); 
+    $('#combineButton').click(function () {
+        $('#messageList').combineMessages();
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind split method to the message handler
-    $('#splitButton').click(function(){
-        $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage()
-        .splitMessage().relayMessage();
+    $('#splitButton').click(function () {
+        $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage()
+            .splitMessage().relayMessage();
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind checksum method to the message
-    $('#checksumButton').click(function(){
+    $('#checksumButton').click(function () {
         // get the text
         var input = $('#msg');
         // create a new message from the input text and add a checksum to it
-        var msg = $('<div><span class="text">' + input.val() +'</span></div>').BYOIMessage()
-        .addChecksum().relayMessage();
+        var msg = $('<div><span class="text">' + input.val() + '</span></div>').BYOIMessage()
+            .addChecksum().relayMessage();
         // add the message to the input (update field value)
+
         BYOI.addMessageToContainer(msg, input);
+
         $('#messageList').getSelectedMessages().toggleSelectMessage();
+        $('#msg').val($("#messageList-checksum").children().last().data('text'));
     });
-    
+
     // bind verify checksum method to the message handler
-    $('#verifyButton').click(function(){
+    $('#verifyButton').click(function () {
         // create a new message and verify the checksum
-        var verify = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().verifyChecksum();
+        var verify = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().verifyChecksum();
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind encryption method to the message handler
-    $('#encryptButton').click(function(){
+    $('#encryptButton').click(function () {
         // encrypt the last select
-        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().encryptMessage(
+        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().encryptMessage(
             parseInt(+$('#recipient').val()) // encryption key is the recipient node
         ).relayMessage(); // send to every message handler
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, $('#msg'));
         $('#messageList').getSelectedMessages().toggleSelectMessage();
+        $('#msg').val($("#messageList-encryped").children().last().data('text'));
     });
 
     // bind decryption method to the message handler
-    $('#decryptButton').click(function(){
-        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().decryptMessage(
+    $('#decryptButton').click(function () {
+        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().decryptMessage(
             parseInt(+$('#recipient').val()) // decryption key is the recipient node
         ).relayMessage(); // send to every message handler
         // add the message to the input (update field value)
@@ -95,45 +118,45 @@ $(document).ready(function() {
     });
 
     // bind random number method to the message handler
-    $('#randomButton').click(function(){
+    $('#randomButton').click(function () {
         // add a random number to the last selected element of the message handler
-        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage()
-        .addRandomNumber().relayMessage();
+        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage()
+            .addRandomNumber().relayMessage();
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, $('#msg'));
     });
 
     // bind move up method to the message handler
-    $('#upButton').click(function(){
+    $('#upButton').click(function () {
         //for each selected message in the main message handler
-        $('#messageList').getSelectedMessages().each(function(){
+        $('#messageList').getSelectedMessages().each(function () {
             //get the index of the current element
             var index = $(this).index();
             // if there is some room up
-            if(index > 1){
+            if (index > 1) {
                 // get the previous element in the message handler
-                var previous = $('#messageList').children().eq(index-1);
+                var previous = $('#messageList').children().eq(index - 1);
                 // if it's not selected, swap
-                if(!previous.hasClass('BYOI-selected')){
+                if (!previous.hasClass('BYOI-selected')) {
                     $(this).detach().insertBefore(previous);
                 }
             }
         });
     });
-    
+
     // bind move down method to the message handler
-    $('#downButton').click(function(){
+    $('#downButton').click(function () {
         //for each selected message in the main message handler
-        $($('#messageList').getSelectedMessages().get().reverse()).each(function(){
+        $($('#messageList').getSelectedMessages().get().reverse()).each(function () {
             //get the index of the current element
             var index = $(this).index();
             var length = $(this).parent().children().length;
             // if there is some room down
-            if(index < length - 1){
+            if (index < length - 1) {
                 // get the previous element in the message handler
-                var next = $('#messageList').children().eq(index+1);
+                var next = $('#messageList').children().eq(index + 1);
                 // if it's not selected, swap
-                if(!next.hasClass('BYOI-selected')){
+                if (!next.hasClass('BYOI-selected')) {
                     $(this).detach().insertAfter(next);
                 }
             }
@@ -141,7 +164,7 @@ $(document).ready(function() {
     });
 
     // bind send method to the message handler
-    $('#sendButton').click(function(){
+    $('#sendButton').click(function () {
         // create a new message 
         // NOTE at least one tag with class "text" should be inside the 
         // html of a message, otherwise the content sent will be an 
@@ -150,15 +173,15 @@ $(document).ready(function() {
         // also, notice that because of this, the message is not inserted with 
         // all the html tags provided, but only those that were inside the 
         // tag with the "text" class.
-        var html = '<div><span class="text">'+$('#msg').val()+'</span></div>';
+        var html = '<div><span class="text">' + $('#msg').val() + '</span></div>';
         // sent message to the server
-        $(html).BYOIMessage().send($('#recipient').val()).css('background', 'gold');
+        $(html).BYOIMessage().send($('#recipient').val());
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
 
     // bind close connection method to the message handler
-    $('#closeButton').click(function() {
+    $('#closeButton').click(function () {
         BYOI.connection.close();
     });
 
@@ -167,26 +190,78 @@ $(document).ready(function() {
     //$('#systemMessage').BYOISystemAlert();
     $('#systemMessage').BYOISystemAlert({
         //this is the default behaviour if the onAlert property is not provided
-        onAlert:function(alert){ 
-            $('#systemMessage').html(alert); 
+        onAlert: function (alert) {
+            $('#systemMessage').html(alert);
+            $('#systemMessages').alert();
+            $('#systemMessages').fadeTo(2000, 500).slideUp(500, function () {
+                $('#systemMessages').alert('close');
+            });
+
         }
     });
-    
+
     // create a Message Handler
     $('#messageList').BYOIMessageHandler({
-        accept:function(message){ 
-            return true; 
-        }, 
-        onError:function(msg){console.log(msg);}
+        accept: function (message) {
+            return true;
+        },
+        onError: function (msg) {
+            console.log(msg);
+        }
     });
 
     // create a Message Handler with a filter
-    encrypted = $('<div id="encryptedList">encrypted messages</div>');
-    encrypted.BYOIMessageHandler({
-        accept: function(msg){
+    toAndFrom = $("#messageBord");
+    toAndFrom.BYOIMessageHandler({
+        accept: function (msg) {
+            if (msg.data('type') == "MESSAGE") {
+                var footer = "";
+                if (msg.data('to') == 0) {
+                    footer = "Bordcast messge";
+                } else {
+                    footer = "send to " + msg.data('to')
+                }
+                var dataCopy = $(msg).data();
+                var newMessage = "  <a href=\"#\" class=\"list-group-item\">"
+                    + "    <h4 class=\"list-group-item-heading\">" + msg.data('text') + "</h4>"
+                    + "    <p class=\"list-group-item-text\">" + footer + "</p>"
+                    + "  </a> ";
+                msg.html(newMessage);
+                msg.addMetadata(dataCopy);
+
+                return true;
+            } else if (msg.hasClass('received')) {
+                var dataCopy = $(msg).data();
+                var newMessage = "  <a href=\"#\" class=\"list-group-item received BYOI-message\">"
+                    + "    <h4 class=\"list-group-item-heading text\">" + msg.data('text') + "</h4>"
+                    + "    <p class=\"list-group-item-text connected\">" + msg.data('node') + "</p>"
+                    + "  </a> ";
+                msg.html(newMessage);
+                msg.addMetadata(dataCopy);
+                return true;
+            } else if (msg.hasClass('added')) {
+                var dataCopy = $(msg).data();
+                var newMessage = "  <a href=\"#\" class=\"list-group-item added BYOI-message\">"
+                    + "    <h4 class=\"list-group-item-heading text\">" + msg.data('text') + "</h4>"
+                    + "    <p class=\"list-group-item-text connected\">" + "self add message" + "</p>"
+                    + "  </a> ";
+                msg.html(newMessage);
+                msg.addMetadata(dataCopy);
+                return true;
+            }
+            return false;
+        }
+    });
+
+    encryptedList = $('#messageList-encryped').BYOIMessageHandler({
+        accept: function (msg) {
             return msg.hasClass('encrypted');
         }
     });
-    // add the second message handler to the DOM
-    $('#sidebar').append(encrypted);
+
+    checksumList = $('#messageList-checksum').BYOIMessageHandler({
+        accept: function(msg){
+            return msg.hasClass('checksum');
+        }
+    });
 });
--- Script/bindings.js
+++ Script/bindings.js
@@ -1,18 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //bind elements of the DOM to BYOI methods
-
-    $('#connectButton').click(function () {
+    
+    $('#connectButton').click(function(){
         // attempt connection to the server
         BYOI.connect();
     });
 
 
-    $('#deleteButton').click(function () {
+    $('#deleteButton').click(function(){
         // delete all selected messages from the main Message Handler
-        $('#messageBord').getSelectedMessages().remove();
+        $('#messageList').getSelectedMessages().remove();
     });
 
-    $('#addButton').click(function () {
+    $('#addButton').click(function(){
         //this is just an example, the final html should be defined by the 
         //student, all the meta-data should be added here though
         //
@@ -20,22 +20,21 @@ $(document).ready(function () {
         //add the meta-data to it. But the API provides a helper method
         //to create the message in a nicer way. An example on how to 
         //use this part of the API is shown in the connection.js file
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
         var msg = $('<div class="added"><span class="text">' + $('#msg').val() + '</span>&nbsp;</div>').BYOIMessage();
         msg.relayMessage()
             .css('background', 'green')
             .animate(
-                {'background-color': 'transparent'},
-                'slow',
-                'swing',
-                function () {
-                    $(this).removeAttr('style');
-                });
+                {'background-color':'transparent'}, 
+                'slow', 
+                'swing', 
+                function(){$(this).removeAttr('style');
+            });
     });
 
 
     // enable click selection of messages
-    $(document).on('click', '.BYOI-message', function () {// bind to every BYOI message, regardless of their Message Handler
+    $(document).on('click', '.BYOI-message', function(){// bind to every BYOI message, regardless of their Message Handler
         // toggle selection
         $(this).toggleSelectMessage();
         // set the input value to the selected message's text 
@@ -43,108 +42,98 @@ $(document).ready(function () {
     });
 
     // bind combine method to the message handler
-    $('#combineButton').click(function () {
-        $('#messageBord').combineMessages();
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+    $('#combineButton').click(function(){
+        $('#messageList').combineMessages(); 
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind split method to the message handler
-    $('#splitButton').click(function () {
-
-        var input = $('#msg').val();
-
-        $('#spiltMessageTextEditor').html("");
-
-        var chars = input.split("");
-        for (var i = 0; i < chars.length; ++i) {
-            var object = "<span>" + chars[i] + "</span>";
-            $('#spiltMessageTextEditor').append(object);
-        }
-        updateSplitMessage();
-
-        $('#splitModal').modal('toggle');
+    $('#splitButton').click(function(){
+        $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage()
+        .splitMessage().relayMessage();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind checksum method to the message
-    $('#checksumButton').click(function () {
+    $('#checksumButton').click(function(){
         // get the text
         var input = $('#msg');
         // create a new message from the input text and add a checksum to it
-        var msg = $('<div><span class="text">' + input.val() + '</span></div>').BYOIMessage()
-            .addChecksum().relayMessage();
+        var msg = $('<div><span class="text">' + input.val() +'</span></div>').BYOIMessage()
+        .addChecksum().relayMessage();
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, input);
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
-
+    
     // bind verify checksum method to the message handler
-    $('#verifyButton').click(function () {
+    $('#verifyButton').click(function(){
         // create a new message and verify the checksum
-        var verify = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().verifyChecksum();
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+        var verify = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().verifyChecksum();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind encryption method to the message handler
-    $('#encryptButton').click(function () {
+    $('#encryptButton').click(function(){
         // encrypt the last select
-        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().encryptMessage(
+        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().encryptMessage(
             parseInt(+$('#recipient').val()) // encryption key is the recipient node
         ).relayMessage(); // send to every message handler
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, $('#msg'));
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind decryption method to the message handler
-    $('#decryptButton').click(function () {
-        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().decryptMessage(
+    $('#decryptButton').click(function(){
+        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage().decryptMessage(
             parseInt(+$('#recipient').val()) // decryption key is the recipient node
         ).relayMessage(); // send to every message handler
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, $('#msg'));
-        $('#messageBord').getSelectedMessages().toggleSelectMessage();
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
     // bind random number method to the message handler
-    $('#randomButton').click(function () {
+    $('#randomButton').click(function(){
         // add a random number to the last selected element of the message handler
-        var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage()
-            .addRandomNumber().relayMessage();
+        var msg = $('<div><span class="text">' + $('#msg').val() +'</span></div>').BYOIMessage()
+        .addRandomNumber().relayMessage();
         // add the message to the input (update field value)
         BYOI.addMessageToContainer(msg, $('#msg'));
     });
 
     // bind move up method to the message handler
-    $('#upButton').click(function () {
+    $('#upButton').click(function(){
         //for each selected message in the main message handler
-        $('#messageBord').getSelectedMessages().each(function () {
+        $('#messageList').getSelectedMessages().each(function(){
             //get the index of the current element
             var index = $(this).index();
             // if there is some room up
-            if (index > 1) {
+            if(index > 1){
                 // get the previous element in the message handler
-                var previous = $('#messageBord').children().eq(index - 1);
+                var previous = $('#messageList').children().eq(index-1);
                 // if it's not selected, swap
-                if (!previous.hasClass('BYOI-selected')) {
+                if(!previous.hasClass('BYOI-selected')){
                     $(this).detach().insertBefore(previous);
                 }
             }
         });
     });
-
+    
     // bind move down method to the message handler
-    $('#downButton').click(function () {
+    $('#downButton').click(function(){
         //for each selected message in the main message handler
-        $($('#messageBord').getSelectedMessages().get().reverse()).each(function () {
+        $($('#messageList').getSelectedMessages().get().reverse()).each(function(){
             //get the index of the current element
             var index = $(this).index();
             var length = $(this).parent().children().length;
             // if there is some room down
-            if (index < length - 1) {
+            if(index < length - 1){
                 // get the previous element in the message handler
-                var next = $('#messageBord').children().eq(index + 1);
+                var next = $('#messageList').children().eq(index+1);
                 // if it's not selected, swap
-                if (!next.hasClass('BYOI-selected')) {
+                if(!next.hasClass('BYOI-selected')){
                     $(this).detach().insertAfter(next);
                 }
             }
@@ -152,37 +141,8 @@ $(document).ready(function () {
     });
 
     // bind send method to the message handler
-    $('#sendButton').click(function () {
-        if ($(this).hasClass('send')) {
-            var html = '<div><span class="text">' + $('#msg').val() + '</span></div>';
-
-            var message = $(html).BYOIMessage().send($('#recipient').val());
-            var dataCopy = message.data();
-
-
-            var footer = "";
-            if (message.data("to") == 0) { // broadcast message
-                footer = 'Broadcast Message';
-            } else { // regular message
-                footer = '<div class="sent">Sent To:<span class="node">' + message.data("to") + '</span></div>';
-            }
-
-            var newHTMLforMessage = "<a href=\"#\" class=\"list-group-item\">"
-                + "<div class=\"list-group-item-text broadcast\">"
-                + "    <div class=\"h2 text-justify text\">" + message.data('text') + "</div>"
-                + "<span class=\"small\">" + footer + "</span>"
-                + "</div>"
-                + "</a>";
-
-            message.html(newHTMLforMessage);
-            message.addMetadata(message);
-            $('#messageBord').getSelectedMessages().toggleSelectMessage();
-
-        } else {
-            openSplitMessage();
-        }
-
-        // create a new message
+    $('#sendButton').click(function(){
+        // create a new message 
         // NOTE at least one tag with class "text" should be inside the 
         // html of a message, otherwise the content sent will be an 
         // empty string
@@ -190,12 +150,15 @@ $(document).ready(function () {
         // also, notice that because of this, the message is not inserted with 
         // all the html tags provided, but only those that were inside the 
         // tag with the "text" class.
-
+        var html = '<div><span class="text">'+$('#msg').val()+'</span></div>';
+        // sent message to the server
+        $(html).BYOIMessage().send($('#recipient').val()).css('background', 'gold');
+        $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
 
     // bind close connection method to the message handler
-    $('#closeButton').click(function () {
+    $('#closeButton').click(function() {
         BYOI.connection.close();
     });
 
@@ -204,25 +167,23 @@ $(document).ready(function () {
     //$('#systemMessage').BYOISystemAlert();
     $('#systemMessage').BYOISystemAlert({
         //this is the default behaviour if the onAlert property is not provided
-        onAlert: function (alert) {
-            $('#systemMessage').html(alert);
+        onAlert:function(alert){ 
+            $('#systemMessage').html(alert); 
         }
     });
-
+    
     // create a Message Handler
-    $('#messageBord').BYOIMessageHandler({
-        accept: function (message) {
-            return true;
-        },
-        onError: function (msg) {
-            console.log(msg);
-        }
+    $('#messageList').BYOIMessageHandler({
+        accept:function(message){ 
+            return true; 
+        }, 
+        onError:function(msg){console.log(msg);}
     });
 
     // create a Message Handler with a filter
     encrypted = $('<div id="encryptedList">encrypted messages</div>');
     encrypted.BYOIMessageHandler({
-        accept: function (msg) {
+        accept: function(msg){
             return msg.hasClass('encrypted');
         }
     });
