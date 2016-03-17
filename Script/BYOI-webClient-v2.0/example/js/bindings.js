bea40f35fad004802d4ee309d326fc7596e807e7
bartal veyhe <bartal35@gmail.com>
2016-03-17 04:21:11 +0000

    Add in the updateing of the wheel, so that it when an message is seleted it can change tasks

--- Script/BYOI-webClient-v2.0/example/js/bindings.js
+++ Script/BYOI-webClient-v2.0/example/js/bindings.js
@@ -96,10 +96,9 @@ $(document).ready(function () {
         }).on('mousemove', '.list-group-item', function () {
             avalibleToJoinDarg = true;
         });
-
     });
 
-    // bind checksum method to the message
+// bind checksum method to the message
     $('#checksumButton').click(function () {
         // get the text
         var input = $('#msg');
@@ -114,14 +113,14 @@ $(document).ready(function () {
         $('#msg').val($("#messageList-checksum").children().last().data('text'));
     });
 
-    // bind verify checksum method to the message handler
+// bind verify checksum method to the message handler
     $('#verifyButton').click(function () {
         // create a new message and verify the checksum
         var verify = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().verifyChecksum();
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
-    // bind encryption method to the message handler
+// bind encryption method to the message handler
     $('#encryptButton').click(function () {
         // encrypt the last select
         var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().encryptMessage(
@@ -133,7 +132,7 @@ $(document).ready(function () {
         $('#msg').val($("#messageList-encryped").children().last().data('text'));
     });
 
-    // bind decryption method to the message handler
+// bind decryption method to the message handler
     $('#decryptButton').click(function () {
         var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage().decryptMessage(
             parseInt(+$('#recipient').val()) // decryption key is the recipient node
@@ -143,7 +142,7 @@ $(document).ready(function () {
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
-    // bind random number method to the message handler
+// bind random number method to the message handler
     $('#randomButton').click(function () {
         // add a random number to the last selected element of the message handler
         var msg = $('<div><span class="text">' + $('#msg').val() + '</span></div>').BYOIMessage()
@@ -153,15 +152,15 @@ $(document).ready(function () {
     });
 
 
-    // bind send method to the message handler
+// bind send method to the message handler
     $('#sendButton').click(function () {
-        // create a new message 
-        // NOTE at least one tag with class "text" should be inside the 
-        // html of a message, otherwise the content sent will be an 
+        // create a new message
+        // NOTE at least one tag with class "text" should be inside the
+        // html of a message, otherwise the content sent will be an
         // empty string
         //
-        // also, notice that because of this, the message is not inserted with 
-        // all the html tags provided, but only those that were inside the 
+        // also, notice that because of this, the message is not inserted with
+        // all the html tags provided, but only those that were inside the
         // tag with the "text" class.
         var html = '<div><span class="text">' + $('#msg').val() + '</span></div>';
         // sent message to the server
@@ -171,13 +170,13 @@ $(document).ready(function () {
     });
 
 
-    // bind close connection method to the message handler
+// bind close connection method to the message handler
     $('#closeButton').click(function () {
         BYOI.connection.close();
     });
 
 
-    // bind combine method to the message handler
+// bind combine method to the message handler
     $('#combineButton').click(function () {
 
         $('#toJoin').empty();
@@ -208,7 +207,7 @@ $(document).ready(function () {
         $('#messageList').getSelectedMessages().toggleSelectMessage();
     });
 
-    // bind split method to the message handler
+// bind split method to the message handler
     $('#splitButton').click(function () {
         var input = $('#msg').val();
 
@@ -289,8 +288,8 @@ $(document).ready(function () {
     }
 
 
-    // create a System Alert
-    //$('#systemMessage').BYOISystemAlert();
+// create a System Alert
+//$('#systemMessage').BYOISystemAlert();
     $('#systemMessage').BYOISystemAlert({
         //this is the default behaviour if the onAlert property is not provided
         onAlert: function (alert) {
@@ -302,7 +301,7 @@ $(document).ready(function () {
         }
     });
 
-    // create a Message Handler
+// create a Message Handler
     $('#messageList').BYOIMessageHandler({
         accept: function (message) {
             return true;
@@ -312,7 +311,7 @@ $(document).ready(function () {
         }
     });
 
-    // create a Message Handler with a filter
+// create a Message Handler with a filter
     toAndFrom = $("#messageBord");
     toAndFrom.BYOIMessageHandler({
         accept: function (msg) {
@@ -379,6 +378,15 @@ $(document).ready(function () {
     taskList = $('#messageList-task').BYOIMessageHandler({
         accept: function (msg) {
             return msg.hasClass('task');
+        }
+    });
+
+
+    $('#messageBord').on('click', '.BYOI-message', function () {
+        if ($('#messageBord').getSelectedMessages().length > 0) {
+            $(encryptionTspan).text('Decrypt');
+        } else {
+            $(encryptionTspan).text('Encrypt');
         }
     });
 });
